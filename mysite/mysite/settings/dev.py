b77e101ebbf9cd4957a7f1807c4833ee364b99f1
Panan Chaimongkol <nnuna2012@gmail.com>
2023-08-31 09:53:52 +0700

    feat: add DB and wagtail template

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-iu2^i1tjl#hz3ud1dv02guy9ch-(7%5n)i!rsw(yx&$9z#uu=n"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
